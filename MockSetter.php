c2bf283acf8b03ac5120ef76ade9838f7f7c1a96
danrspencer <danrspen@gmail.com>
2014-03-14 10:14:52 +0000

    MockSetter updated to allow easy retraining of mocks

--- MockSetter.php
+++ MockSetter.php
@@ -15,22 +15,16 @@ class MockSetter {
 
     function toReturn($value)
     {
-        $matcher = new PHPUnit_Framework_MockObject_Matcher_AnyInvokedCount();
         $stub = new PHPUnit_Framework_MockObject_Stub_Return($value);
 
-        $this->mock->expects($matcher)
-                   ->method($this->functionName)
-                   ->will($stub);
+        $this->trainMock($stub);
     }
 
     function toCallFake($callback)
     {
-        $matcher = new PHPUnit_Framework_MockObject_Matcher_AnyInvokedCount();
         $stub = new PHPUnit_Framework_MockObject_Stub_ReturnCallback($callback);
 
-        $this->mock->expects($matcher)
-                   ->method($this->functionName)
-                   ->will($stub);
+        $this->trainMock($stub);
     }
 
 
@@ -40,5 +34,50 @@ class MockSetter {
 
         $this->mock->expects($matcher)
                    ->method($this->functionName);
+    }
+
+    private function trainMock($stub)
+    {
+        if ($this->overwriteExistingStub($stub) === false) {
+            $matcher = new PHPUnit_Framework_MockObject_Matcher_AnyInvokedCount();
+
+            $this->mock->expects($matcher)
+                        ->method($this->functionName)
+                        ->will($stub);
+        }
+    }
+
+    private function overwriteExistingStub($stub)
+    {
+        $invocationMocker = $this->getPrivateProperty($this->mock, '__phpunit_invocationMocker');
+
+        if ($invocationMocker === null) {
+            return false;
+        }
+
+        $matchers = $this->getPrivateProperty($invocationMocker, 'matchers');
+
+        foreach ($matchers as $matcher) {
+            $methodNameMatcher = $matcher->methodNameMatcher;
+            $constraint = $this->getPrivateProperty($methodNameMatcher, 'constraint');
+            $methodName = $this->getPrivateProperty($constraint, 'value');
+
+            if ($methodName === $this->functionName) {
+                $matcher->stub = $stub;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function getPrivateProperty($object, $propertyName)
+    {
+        $reflect = new ReflectionClass($object);
+        $property = $reflect->getProperty($propertyName);
+        $property->setAccessible(true);
+
+        return $property->getValue($object);
     }
 } 

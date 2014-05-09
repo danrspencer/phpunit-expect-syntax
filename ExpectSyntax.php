7b2ddce9718beb326ff8208daea50dd382dd4949
danrspencer <danrspen@gmail.com>
2014-05-09 11:30:55 +0100

    Removed mock functionality from the expect syntax

--- ExpectSyntax.php
+++ ExpectSyntax.php
@@ -1,14 +1,8 @@
 <?php
 
 require_once 'Expector.php';
-require_once 'MockSetter.php';
 
 function expect($actual, $functionName = null)
 {
     return new Expector($actual, $functionName);
-}
-
-function setMock(\PHPUnit_Framework_MockObject_MockObject $mock, $functionName)
-{
-    return new MockSetter($mock, $functionName);
 }

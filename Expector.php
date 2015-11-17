c39794d64e52ccb32166392d6fb3f5049abef897
Spencer, Daniel <Daniel.Spencer2@bskyb.com>
2015-11-17 10:48:37 +0000

    Added toContainAll and ordered functions alphabetically in the expector

--- Expector.php
+++ Expector.php
@@ -21,29 +21,14 @@ class Expector
         \PHPUnit_Framework_Assert::assertArrayNotHasKey($key, $this->actual);
     }
 
-    public function toEqual($expected)
+    public function classToHaveAttribute($attributeName)
     {
-        \PHPUnit_Framework_Assert::assertEquals($expected, $this->actual);
-    }
-    
-    public function toNotEqual($expected)
-    {
-        \PHPUnit_Framework_Assert::assertNotEquals($expected, $this->actual);
+        \PHPUnit_Framework_Assert::assertClassHasAttribute($attributeName, $this->actual);
     }
 
-    public function toHaveAttribute($attributeName)
+    public function toBe($expected)
     {
-        \PHPUnit_Framework_Assert::assertObjectHasAttribute($attributeName, $this->actual);
-    }
-    
-    public function toNotHaveAttribute($attributeName)
-    {
-        \PHPUnit_Framework_Assert::assertObjectNotHasAttribute($attributeName, $this->actual);
-    }
-
-    public function toBeTrue()
-    {
-        \PHPUnit_Framework_Assert::assertTrue($this->actual);
+        \PHPUnit_Framework_Assert::assertSame($expected, $this->actual);
     }
 
     public function toBeFalse()
@@ -51,38 +36,58 @@ class Expector
         \PHPUnit_Framework_Assert::assertFalse($this->actual);
     }
 
-    public function toMatch($regex)
+    public function toBeGreaterThan($expected)
     {
-        \PHPUnit_Framework_Assert::assertRegExp($regex, $this->actual);
-    }
-
-    public function toContain($needle)
-    {
-        \PHPUnit_Framework_Assert::assertContains($needle, $this->actual);
-    }
-    
-    public function toBe($expected)
-    {
-        \PHPUnit_Framework_Assert::assertSame($expected, $this->actual);
+        \PHPUnit_Framework_Assert::assertGreaterThan($expected, $this->actual);
     }
 
     public function toBeInstanceOf($expected)
     {
         \PHPUnit_Framework_Assert::assertInstanceOf($expected, $this->actual);
     }
-    
-    public function classToHaveAttribute($attributeName)
-    {
-        \PHPUnit_Framework_Assert::assertClassHasAttribute($attributeName, $this->actual);
-    }
 
-    public function toBeGreaterThan($expected)
-    {
-        \PHPUnit_Framework_Assert::assertGreaterThan($expected, $this->actual);
-    }
-    
     public function toBeLessThan($expected)
     {
         \PHPUnit_Framework_Assert::assertLessThan($expected, $this->actual);
+    }
+
+    public function toBeTrue()
+    {
+        \PHPUnit_Framework_Assert::assertTrue($this->actual);
+    }
+
+    public function toContain($needle)
+    {
+        \PHPUnit_Framework_Assert::assertContains($needle, $this->actual);
+    }
+
+    public function toContainAll($expected)
+    {
+        \PHPUnit_Framework_Assert::assertEquals($expected, $this->actual, "\$canonicalize = true", $delta = 0.0, $maxDepth = 10, $canonicalize = true);
+    }
+
+    public function toEqual($expected)
+    {
+        \PHPUnit_Framework_Assert::assertEquals($expected, $this->actual);
+    }
+
+    public function toHaveAttribute($attributeName)
+    {
+        \PHPUnit_Framework_Assert::assertObjectHasAttribute($attributeName, $this->actual);
+    }
+
+    public function toMatch($regex)
+    {
+        \PHPUnit_Framework_Assert::assertRegExp($regex, $this->actual);
+    }
+
+    public function toNotEqual($expected)
+    {
+        \PHPUnit_Framework_Assert::assertNotEquals($expected, $this->actual);
+    }
+
+    public function toNotHaveAttribute($attributeName)
+    {
+        \PHPUnit_Framework_Assert::assertObjectNotHasAttribute($attributeName, $this->actual);
     }
 }
